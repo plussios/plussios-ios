23668f931a2579cf862fb5a27857d9393ae54e94
Stan Sidel <stansidel@gmail.com>
2024-01-03 13:39:49 +0600

    Sentry integration

--- Plussios/App/PlussiosApp.swift
+++ Plussios/App/PlussiosApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import Sentry
+
 
 @main
 struct PlussiosApp: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://080dae8b56c8c9bddf71a451884137e0@o1027320.ingest.sentry.io/4506506161881088"
+            options.debug = true // Enabled debug when first installing is always helpful
+            options.enableTracing = true 
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+        }
+    }
     var body: some Scene {
         WindowGroup {
             SettingsView(
